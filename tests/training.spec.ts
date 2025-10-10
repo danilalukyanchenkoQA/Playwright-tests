dc7e3d4242950953062475c6d3064dc9f66228df
Лукьянченко Данила <116444575+danilalukyanchenkoQA@users.noreply.github.com>
2025-10-10 17:42:57 +0300

    Add files via upload

---
+++ tests/training.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test('Проверка заголовка', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await expect(page).toHaveTitle(/Grafana/);
+});
+
+test('Нажатие на кнопку Forgot your password', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+
+  // Click the Forgot your password link.
+  await page.getByRole('link', { name: 'Forgot your password?' }).click();
+
+  // Expects page to have a button with the name of Send reset email.
+  await expect(page.getByRole('button', { name: 'Send reset email' })).toBeVisible();
+});
+
+test('Вход с невалидным логином и паролем', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.locator('[data-testid="data-testid Username input field"]').fill('Логин');
+  await page.locator('[data-testid="data-testid Password input field"]').fill('Пароль');
+  await page.locator('[data-testid="data-testid Login button"]').click();
+  await expect(page.getByText('Invalid username or password')).toBeVisible();
+});
+
+
+
+
+
+
+test.skip('Мой первый тест1', async () => {
+  console.log('Hello, Inzhenerka.Tech!')
+});
+
+test.skip('Мой первый тест2', () => {
+  console.log('Hello, Inzhenerka.Tech!');
+});
+
